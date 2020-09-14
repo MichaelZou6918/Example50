8a6b8260f08acc158c8dad8c871257b47c36d785
zou <zou@10.32.12.5>
2020-09-14 12:47:06 +0800

    有5个人坐在一起，问第五个人多少岁？他说比第4个人大2岁。问第4个人岁数，他说比第3个人大2岁。问第三个人，又说比第2人大两岁。问第2个人，说比第一个人大两岁。最后问第一个人，他说是10岁。请问第五个人多大？

---
+++ src/exa23/Exa23.java
@@ -0,0 +1,15 @@
+package exa23;
+
+public class Exa23 {
+	public static void main(String args[])
+	{
+		int x=10;
+		int y;
+		for (int i=1;i<5;i++)
+		{
+			x+=2;
+		}
+
+		System.out.println(x);
+	}
+}
