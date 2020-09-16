64e7cfd079eb266f53b87eebdb5b358df7ae74d1
zou <zou@10.32.12.5>
2020-09-16 17:37:15 +0800

    809*??=800*??+9*?? 其中??代表的两位数,8*??的结果为两位数，9*??的结果为3位数。求??代表的两位数，及809*??后的结果。

---
+++ src/exa42/Exa42.java
@@ -0,0 +1,16 @@
+package exa42;
+
+public class Exa42 {
+	public static void main(String args[])
+	{
+		for(int i=10;i<=99;i++)
+		{
+			if(809*i==800*i+9*i&&i*9<=999)
+			{
+				System.out.println(i);
+				System.out.println(809*i);
+
+			}
+		}
+	}
+}
